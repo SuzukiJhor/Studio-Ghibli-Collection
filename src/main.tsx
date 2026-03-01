9aef5f5ce13fb2fe48c228fcdb30727c59885f36
jhordan.suzuki <jhordan.kendy1@gmail.com>
2026-03-01 13:45:44 -0300

    feat(theme): adaptations for dark/light theme toggle

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider } from './provider/ThemeProvider.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <main>
-      <App />
-    </main>
+    <ThemeProvider>
+      <main>
+        <App />
+      </main>
+    </ThemeProvider>
   </StrictMode>,
 )
